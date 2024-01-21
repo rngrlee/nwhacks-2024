cf5efa4c96d2cadb145d6c3f8451e186d58bae64
yood2 <yood2@proton.me>
2024-01-21 09:12:43 -0800

    asd

--- lokal/src/pages/Inbox.jsx
+++ lokal/src/pages/Inbox.jsx
@@ -3,15 +3,37 @@ import Navbar from "../Navbar"
 export default function Add() {
 
     return (
-        <div>
-            <h1 className="title">Inbox (1)</h1>
-            <ul class="chats">
-                <li>
-                    <img src='https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg' alt="Profile" className="profile-image" />
-                    <a href="/chat">Ranger Lee</a>
-                </li>
-            </ul>
+        <>
+            <div class="chat-container">
+                <ul class="chat-list">
+                    <li class="chat-item">
+                        <img src="https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg" alt="Profile 1" class="profile-image"></img>
+                        <span class="chat-name">Alice</span>
+                    </li>
+                    <li class="chat-item">
+                        <img src="https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg" alt="Profile 2" class="profile-image"></img>
+                        <span class="chat-name">Bob</span>
+                    </li>
+                </ul>
+                
+            </div>
             <Navbar />
-        </div>
+        </>
+
+        /* // <div>
+        //     <h1 className="title">Inbox (1)</h1>
+        //     <ul class="chats">
+        //         <li>
+        //             <img src='https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg' alt="Profile" className="chat-profile" />
+        //             <a href="/chat">Ranger Lee</a>
+        //         </li>
+        //         <li></li>
+
+        //         <li></li>
+        //         <li></li>
+        //         <li></li>
+        //     </ul>
+        //     <Navbar />
+        // </div> */
     )
 }
