ac53ab8a8e0bf38414a4c932e50c4cbf94afe4c2
Ranger Lee <mrrangerlee@gmail.com>
2024-01-21 11:01:10 -0800

    fixed chat

--- lokal/src/pages/Chat.jsx
+++ lokal/src/pages/Chat.jsx
@@ -7,10 +7,10 @@ const Chat = () => {
 
     const [messages, setMessages] = useState([])
 
-    const getMessages = async () => {
-        const returnedMessages = await messageService.getAll()
+    const getMessages =  () => {
+        const returnedMessages =  messageService.getAll().then((res) => setMessages(returnedMessages))
         console.log(returnedMessages, 'returned')
-        setMessages(returnedMessages)
+        // setMessages(returnedMessages)
     }
 
     useEffect(() => {
@@ -23,13 +23,13 @@ const Chat = () => {
                 <h3 className="name">Ranger Lee</h3>
                 <h4 className='active'>Active 3 mins ago</h4>
                 <div className='msg-container'>
-                    <div className='time time-1'>{messages[0].time}</div>
+                    <div className='time time-1'>Mon, Aug 16 2023 at 8:54 PM</div>
                     <div className='first-msg'>
-                        {messages[0].content.slice(7)}
+                    Are you still looking for a caretaker? I have a couple plants of my own and can be available in your requested time frame :)
                     </div>
-                    <div className='time'>{messages[1].time}</div>
+                    <div className='time'>9:41 PM</div>
                     <div className='second-msg'>
-                        {messages[1].content.slice(7)}
+                        Hi Ranger, yes I am still looking. Can you give me a quick call at 604-333-7874 sometime tomorrow?
                     </div>
                     <div className='message-bar'>
                         <input className='message-input'type="text" placeholder='   Message...' />
