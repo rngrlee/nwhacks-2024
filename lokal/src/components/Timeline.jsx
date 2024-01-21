80798d822b35c88b34259213e5ef3eef9ec1ca25
Ranger Lee <mrrangerlee@gmail.com>
2024-01-21 00:24:27 -0800

    added timeline component

--- lokal/src/components/Timeline.jsx
+++ lokal/src/components/Timeline.jsx
@@ -1,8 +1,33 @@
+import Post from "./Post"
+import postService from '../services/posts'
+
 const Timeline = () => {
+
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+        const postsReturned = await postService.getAll()
+        setPosts(postsReturned)
+    }
+
+    getPosts()
 
     return (
         <>
-    
+            {posts.map(post => {
+                <Post 
+                key={post.id} 
+                title={post.title} 
+                location={post.location} 
+                daysAgo={post.daysAgo} 
+                postUser={post.postUser} 
+                type={post.type} 
+                startDay={post.startDay} 
+                endDay={post.endDay}
+                payAmount={post.payAmount} 
+                intersection={post.intersection} 
+                tasks={post.tasks} />
+            })}
         </>
     )
 }
