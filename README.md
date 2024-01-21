707399e96e3c02d68bfac30b79b9825382218994
yood2 <yood2@proton.me>
2024-01-21 01:40:12 -0800

    rebasing

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 1. cd into `lokal`
 2. `npm install`
 3. `npm install react-router-dom`
+4. `npm `
 4. `npm run dev`
 5. `npm run server`
---
+++ originalApp.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react"
+
+import loginService from './services/login'
+import Profile from './components/Profile'
+import LoginForm from './components/LoginForm';
+import FooterMenu from "./components/Footer";
+
+function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      const usernames = user.map(u => u.username)
+      const passwords = user.map(u => u.password)
+
+      const equalPos = usernames.indexOf(username) === passwords.indexOf(password)
+
+      if (usernames.includes(username) && passwords.includes(password) && equalPos) {
+        window.localStorage.setItem(
+          'loggedAppUser', JSON.stringify(user)
+        )
+        setUser(user)
+        setUsername('')
+        setPassword('')
+        }
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <>
+    <h2>Log in</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          password
+          <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>      
+  )
+
+  return (
+    <>
+      <div>
+      {user === null
+        ? <LoginForm
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            handleLogin={handleLogin}
+          />
+        : <Profile picture='https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg' name="LeBron James"/>
+      }
+      </div>
+      <div>
+        <FooterMenu />
+      </div>
+    </>
+  )
+}
+
+export default App
