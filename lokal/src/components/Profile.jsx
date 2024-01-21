53df19c0cdf35221d3c5df311d1a7ced61269536
Ranger Lee <mrrangerlee@gmail.com>
2024-01-20 17:35:20 -0800

    updated profile component

--- lokal/src/components/Profile.jsx
+++ lokal/src/components/Profile.jsx
@@ -1,13 +1,19 @@
-const Profile = ({ picture, name, bday, email, location, bio }) => {
+const Profile = ({ picture, name }) => {
 
     return (
         <>
-            {picture}
+            <h1>Profile</h1>
+            <img src={picture} alt="" />
             <h3>{name}</h3>
-            DOB: {bday} <br />
-            email: {email} <br />
-            location: {location} <br />
-            bio: {bio} <br />
+            <h2>Settings</h2>
+            <button>Personal Information</button> <br />
+            <button>Login & Security</button> <br />
+            <button>Accessiblity</button> <br />
+            <button>Notifications</button> <br />
+            <button>Privacy & Sharing</button> <br />
+            <h2>Legal</h2>
+            <button>Terms of Service</button> <br />
+            <button>Privacy Policy</button> <br />
         </>
     )
 }
