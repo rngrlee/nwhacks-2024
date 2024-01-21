bcab81b5ffde4fa0417cc698cab4f4f2a4ea9b07
yood2 <yood2@proton.me>
2024-01-21 10:16:59 -0800

    asd

--- lokal/src/pages/Inbox.jsx
+++ lokal/src/pages/Inbox.jsx
@@ -5,17 +5,26 @@ export default function Add() {
     return (
         <>
             <div class="chat-container">
-                <ul class="chat-list">
-                    <li class="chat-item">
-                        <img src="https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg" alt="Profile 1" class="profile-image"></img>
-                        <span class="chat-name">Alice</span>
-                    </li>
-                    <li class="chat-item">
-                        <img src="https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg" alt="Profile 2" class="profile-image"></img>
-                        <span class="chat-name">Bob</span>
-                    </li>
-                </ul>
-                
+                <div className="chat-item">
+                    <h1 className="title">Inbox (1)</h1>
+                    <img src="https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg" alt="Profile 1" class="profile-image"></img>
+                    <a href="/chat">
+                        <p>Ranger Lee</p>
+                    </a>
+                    <p>asdkjhaskdjhaskjdashkjahkj</p>
+                </div>
+                <div className="chat-item">
+                    <h1 className="title">Inbox (1)</h1>
+                    <img src="https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg" alt="Profile 1" class="profile-image"></img>
+                    <a href="/chat">Ranger Lee</a>
+                    <p>asdkjhaskdjhaskjdashkjahkjdash,skdnas,dkak,asmn</p>
+                </div>
+                <div className="chat-item">
+                    <h1 className="title">Inbox (1)</h1>
+                    <img src="https://sharpfocusphoto.com/wp-content/uploads/2020/08/DSC_0067.jpg" alt="Profile 1" class="profile-image"></img>
+                    <a href="/chat">Ranger Lee</a>
+                    <p>asdkjhaskdjhaskjdashkjahkjdash,skdnas,dkak,asmn</p>
+                </div>
             </div>
             <Navbar />
         </>
