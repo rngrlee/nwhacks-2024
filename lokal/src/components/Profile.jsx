eb5ac5614b15f210c1fd0503948d454423122c46
Ranger Lee <mrrangerlee@gmail.com>
2024-01-20 22:12:25 -0800

    worked on profile css

--- lokal/src/components/Profile.jsx
+++ lokal/src/components/Profile.jsx
@@ -1,20 +1,24 @@
 import './css/Styles.css';
 import './css/Profile.css';
 
-const Profile = ({ picture, name }) => {
+const Profile = ({ picture, name, star }) => {
 
     return (
         <>
             <h1 className="title">Profile</h1>
-            <div class="horizontal-align">
+            <div className="horizontal-align">
                 <img src={picture} alt="Profile" className="profile-image" />
-                <h4 className="profile-name">{name}</h4>
+                <div className='horizontal-child'>
+                    <h4 className="profile-name">{name}</h4>
+                    <button className='change-photo'>Change Photo</button> <br />
+                    ★ {star}/5
+                </div>
             </div>
 
             <br></br>
-            <button>Get Help</button>
-            <button>Reviews</button>
-            <button>Activity</button>
+            <button className='top-buttons'>Get Help</button>
+            <button className='top-buttons'>Reviews</button>
+            <button className='top-buttons'>Activity</button>
 
             <h2>Settings</h2>
             <button>Personal Information</button> <br />
