c81bc3f18ada1a686d27f917607d9e21b66be812
Ranger Lee <mrrangerlee@gmail.com>
2024-01-21 03:37:08 -0800

    added message service and combined json servers

--- lokal/src/pages/Inbox.jsx
+++ lokal/src/pages/Inbox.jsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from "../Navbar"
-import Messages from '../messages.json';
+import messageService from '../services/messages'
 
 function Inbox() {
+
+    const [messages, setMessages] = useState([])
+
+    const getMessages = async () => {
+        const returnedMessages = await messageService.getAll()
+        setMessages(returnedMessages)
+    }
+
+    useEffect(() => {
+        getMessages()
+    }, [])
+    
     return (
         <>
             <div>
                 <h1>Inbox</h1>
                 {
-                    Messages.map( record => {
+                    messages.map( record => {
                         return(
                             <div className="box">
                                 <h4>{ record.sender }</h4>
